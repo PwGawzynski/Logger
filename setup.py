c9c30e455ada4e42666287ebbf919a094de69a99
pwgawzynski <pgawzynski.backup@gmail.com>
2024-07-04 06:23:01 +0200

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="logger",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(),
     install_requires=[],
     author="Paweł Gawżynski && Jakub Wrona",
