c3fdd8483afcc353322435f54cefee6645a2aaf7
pwgawzynski <pgawzynski.backup@gmail.com>
2024-07-06 07:37:03 +0200

    multiprocessing

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="logger",
-    version="0.0.10",
+    version="0.0.11",
     packages=find_packages(),
     install_requires=[
         "colorama==0.4.6",
