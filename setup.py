3f4f2a9733589321e68ff59b4126a6581ef5f617
pwgawzynski <pgawzynski.backup@gmail.com>
2024-08-01 22:26:16 +0200

    updated logic and version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="logger",
-    version="0.0.13",
+    version="0.0.14",
     packages=find_packages(),
     install_requires=[
         "colorama==0.4.6",
